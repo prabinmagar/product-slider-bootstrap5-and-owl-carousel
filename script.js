d488d074144b643375810df88f15ef449428257b
Prabin Magar <psmagar123451@gmail.com>
2021-09-07 16:29:25 +0545

    Source Code

---
+++ script.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    responsiveClass: true,
+    responsive: {
+        0:{
+            items: 1,
+        },
+        768:{
+            items: 2,
+        },
+        1100:{
+            items: 3,
+        },
+        1400:{
+            items: 4,
+            loop: false,
+        }
+    }
+});
